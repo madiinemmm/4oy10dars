996680767307405908808f9b6bf588a0096da13a
madiinemm <uraimovamadina179@gmail.com>
2023-12-15 20:00:09 +0500

    masalalar ishlandi

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// let arr1 = [1,6,9,6];
+// let arr2 = [5,9,5,2];
+// let arr3 = [6,4,0,1];
+// let arr4 = arr3.concat(arr2, arr1);
+// console.log(arr4);
+
+// let arr = [3,7,9, 5, 6, 10,8];
+// let a = arr.findIndex((valeu, index, arr) => {
+//     return valeu % 2 == 0 ** 2 && valeu % 5 == 0 ** 2
+// });
+// console.log(a);
+
+// let arr = [3, -5, 4, -9, 6, 10];
+// let a = arr.find((valeu, index, arr) => {
+//     return index % 2 == 1
+// });
+// console.log(a);
+
+// let arr = [3,7,9, 5, 6, 10,8];
+// let sum = 0
+// let a = arr.findIndex((valeu, index, arr) => {
+//     if(valeu % 2 == 1 && valeu % 3 == 0){
+//        sum = sum + valeu
+//     }
+//         return sum 
+// });
+// console.log(sum);
+
+// let arr = ["hello", "hi", true, false, 4, "bye"];
+// let test = arr.indexOf(6);
+// console.log(test);
